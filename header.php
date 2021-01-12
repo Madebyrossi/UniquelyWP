eac21cf4e73dbd0b0d94db176166e3a2791b0b58
Liliripe <liliripe.studio@gmail.com>
2021-01-12 12:57:52 -0600

    init header updates

--- header.php
+++ header.php
@@ -1,15 +1,3 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package uniquely
- */
-
-?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -17,43 +5,81 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+	<!-- Font CSS -->
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,400;0,500;0,600;0,800;1,400&display=swap" rel="stylesheet">
+
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'uniquely' ); ?></a>
+	<?php wp_body_open(); ?>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$uniquely_description = get_bloginfo( 'description', 'display' );
-			if ( $uniquely_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $uniquely_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+	<div id="page" class="site">
+		<header class="site-header">
+			<div class="topbar">
+				<div class="wrapper">
+					<div class="inner">
+						<div class="newsletter">
+							<a href="#" target="blank">
+								<i class="far fa-envelope"></i>
+								<span>Newsletter</span>
+							</a>
+						</div>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'uniquely' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+						<div class="search">
+							<a href="/search">
+								<i class="fas fa-search"></i>
+								<span>Search</span>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="logo wrapper">
+				<?php the_custom_logo(); ?>
+			</div>
+
+			<div class="menu wrapper">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="inner">
+							<div class="col-md-3 social-links">
+								<a href="#" target="_blank">
+									<i class="fab fa-facebook-f"></i>
+								</a>
+								<a href="#" target="_blank">
+									<i class="fab fa-twitter"></i>
+								</a>
+								<a href="#" target="_blank">
+									<i class="fab fa-instagram"></i>
+								</a>
+							</div>
+
+							<div class="col-md-6 main-menu">
+								<ul>
+									<li>
+										<a href="">Explore</a>
+									</li>
+									<li>
+										<a href="">About Us</a>
+									</li>
+									<li>
+										<a href="">Contact</a>
+									</li>
+								</ul>
+							</div>
+
+							<div class="col-md-3 merchant-link">
+								<i class="far fa-user"></i>
+								<a href="/merchant-signup">Merchant Signup</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
